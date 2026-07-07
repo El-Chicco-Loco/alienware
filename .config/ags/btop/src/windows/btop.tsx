27aa15b3cdec459bdc3b324806d7a99de46ef734
El-Chicco-Loco <lozerfederico@proton.me>
2026-07-07 11:13:48 +0200

    btop working with terminal on floating window

--- .config/ags/btop/src/windows/btop.tsx
+++ .config/ags/btop/src/windows/btop.tsx
@@ -3,30 +3,52 @@ import { QuickSettingsModule } from "../modules/quicksettings/quicksettings";
 import { BarItemPopup } from "@/src/widgets/baritempopup";
 import Adw from "gi://Adw?version=1";
 import { attachHover, handleHover } from "@/src/widgets/baritem";
+import app from "ags/gtk4/app"
+import Gtk from "gi://Gtk?version=4.0"
+import Gio from "gi://Gio"
+import GLib from "gi://GLib"
+import Vte from "gi://Vte?version=3.91"
+import { Astal, Gdk } from "ags/gtk4";
+import { QuickSettings } from "../modules/bar/items/quicksettings";
+import { Popup } from "@/src/widgets/popup";
+import { PowerMenuModule } from "../../../powermenu/src/modules/powermenu/powermenu";
 
 
-export function BtopWindow() {
+export function BtopWindow({
+   gdkmonitor,
+   $,
+}: JSX.IntrinsicElements["window"] & { gdkmonitor: Gdk.Monitor }) {
+   
+   const terminal = new Vte.Terminal({
+      hexpand: true,
+      vexpand: true,
+   })
+
+   const shell = GLib.getenv("SHELL") || "/bin/bash"
+
+   // Correct signature: 9 arguments
+   terminal.spawn_async(
+      Vte.PtyFlags.DEFAULT,      // pty_flags
+      null,                      // working_directory
+      [shell],                   // argv
+      null,                      // envv
+      GLib.SpawnFlags.DEFAULT,   // spawn_flags
+      null,                      // child_setup
+      -1,                        // timeout
+      null,                      // cancellable
+      null                       // callback (must be function or null)
+   )
+   const command = "btop"  // Include \n to execute
+   terminal.feed(command.repeat(1)) 
+   terminal.feed("\n".repeat(1)) 
+
    return (
-      <BarItemPopup
-         name={windows_names.quicksettings}
-         module={"quicksettings"}
-         width={440}
-      >
-         <Adw.Clamp
-            class={"hoverarea"}
-            $={(self) => {
-                        attachHover(self, () => {
-                           handleHover("open-qs-popup");
-                        }, () => {
-                           handleHover("close-qs-popup");
-                        });
-                     }}
-         >
-            <QuickSettingsModule />
-         </Adw.Clamp>
-      </BarItemPopup>
+      <window
+         visible
+         title="AGS Terminal"
+         defaultWidth={800}
+         defaultHeight={600}
+         child={terminal}
+      />
    );
 }
-
-
-x
